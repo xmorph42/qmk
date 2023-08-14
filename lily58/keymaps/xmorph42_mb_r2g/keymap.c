24823b9d022dd0b703b0ebbe5adfc0959af577c4
Peter Busch <peter.busch@povse.de>
2023-08-14 22:50:14 +0200

    Remove the inner (sofle-encoder) keys - they are interrupting me when typing on Windows

--- lily58/keymaps/xmorph42_mb_r2g/keymap.c
+++ lily58/keymaps/xmorph42_mb_r2g/keymap.c
@@ -124,7 +124,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
  * | ESC  |   A  |   R  |   S  |   T  |   G  |-------.    ,-------|   M  |   N  |   E  |   I  |   O  | ' "  |
  * |      |   GUI|   ALT|   SFT|   CTL|      |       |    |       |      |   CTL|   SFT|   ALT|   GUI|      |   
- * |------+------+------+------+------+------|  LGUI |    | RGUI  |------+------+------+------+------+------|
+ * |------+------+------+------+------+------|  n/a  |    | n/a   |------+------+------+------+------+------|
  * |LShift|   Z  |   X  |   C  |   D  |   V  |-------|    |-------|   K  |   H  |  , < |  . > |  / ? |RShift|
  * `-----------------------------------------/       /     \      \-----------------------------------------'
  *            ~SOFLE_L~ LAlt | LCTR |LOWER | /Enter  /      \Space \   |RAISE | RCTR | RAlt ~SOFLE_R~
@@ -135,7 +135,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_GRV , KC_1   , KC_2   , KC_3   , KC_4   , KC_5      ,                     KC_6,       KC_7   , KC_8   , KC_9   , KC_0   , KC_MINS,
   KC_TAB , KC_Q   , KC_W   , KC_F   , KC_P   , KC_B      ,                     KC_J,       KC_L   , KC_U   , KC_Y   , KC_SCLN, KC_BSPC,
 LT(_LOC, KC_ESC) , C_GUI_A, C_ALT_R, C_CTL_S, C_SFT_T, KC_G      ,                     KC_M,       C_SFT_N, C_CTL_E, C_ALT_I, C_GUI_O, LT(_LOC, KC_QUOT),
-  KC_LSFT, KC_Z   , KC_X   , KC_C   , KC_D   , KC_V      , KC_LGUI,   KC_RGUI, KC_K,       KC_H   , KC_COMM, KC_DOT , KC_SLSH, KC_RSFT,
+  KC_LSFT, KC_Z   , KC_X   , KC_C   , KC_D   , KC_V      , XXXXXXX,  XXXXXXX, KC_K,       KC_H   , KC_COMM, KC_DOT , KC_SLSH, KC_RSFT,
                     SOFLE_L  KC_LALT, KC_LCTL, MO(_LOWER), KC_ENT ,   KC_SPC , MO(_RAISE), KC_RCTL, KC_RALT SOFLE_R
 ),
 
